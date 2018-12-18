db8c0b55fad677c41e689969b5838ddf1671704c
QAP <QAP@QAP-PC>
2018-12-18 19:44:53 +0530

    committed on 18th Dec

---
+++ Karthik-Maven/src/main/java/myPack/Karthik.java
@@ -0,0 +1,28 @@
+package myPack;
+import org.testng.annotations.Test;
+public class Karthik {
+@Test
+public static void verifyLogin() {
+	System.out.println("Verify Login success!");
+}
+@Test
+public static void addEmp() {
+	System.out.println("Add emp success!");
+}
+@Test
+public static void editEmp() {
+	System.out.println("Edit emp success!");
+}
+@Test
+public static void deleteEmp() {
+	System.out.println("Delete emp success!");
+}
+@Test
+public static void addLocation() {
+	System.out.println("Add location success!");
+}
+@Test
+public static void editLocation() {
+	System.out.println("Edit location success!");
+}
+}
